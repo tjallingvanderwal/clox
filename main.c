4ab1c4a8bb25d3931d0dbc5768040cc37e8befed
Tjalling van der Wal <tjalling.vanderwal@nedap.com>
2021-12-31 21:24:05 +0100

    chapter 15 - setup vm

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]){
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
@@ -11,9 +14,12 @@ int main(int argc, const char* argv[]){
     writeConstant(&chunk, 1.4, 14);
     writeConstant(&chunk, 1.5, 15);
     writeConstant(&chunk, 1.6, 16);
-
     writeChunk(&chunk, OP_RETURN, 12);
+    
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
