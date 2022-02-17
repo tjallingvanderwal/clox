4da7b57b8f1c1d0ef35fa1c2ab1bbb88a16b7b08
Tjalling van der Wal <tjalling.vanderwal@nedap.com>
2022-02-17 11:26:14 +0100

    cleanup

--- test/features/support/env.rb
+++ test/features/support/env.rb
@@ -1,10 +1,1 @@
 require 'dotenv/load'
-
-AfterStep do
-    if @stdin_stream
-        puts 'closing streams'
-        @stdin_stream.close
-        @stdout_stream.close
-        @stderr_stream.close
-    end
-end
