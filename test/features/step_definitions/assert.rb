d77e15a8c74210ad0f2616617eb6b02d631753dd
Tjalling van der Wal <tjalling.vanderwal@nedap.com>
2022-02-28 10:35:09 +0100

    improve test helpers to prodduce colored diffs for failed expectations

--- test/features/step_definitions/assert.rb
+++ test/features/step_definitions/assert.rb
@@ -1,6 +1,22 @@
 require 'open3'
 require 'active_support/all'
 
+# Reformat actual output and expected output
+# so that tests are less finicky about whitespace.
+#
+# 1. Remove redundant whitespace within lines
+# 2. Remove empty lines
+# 3. Preserve newlines so that RSpec will produce nice diffs 
+def normalize_ws(string)
+    string.lines.map(&:squish)
+                .reject(&:blank?)
+                .join("\n")
+end
+
+def expect_output(actual, expected)
+    expect(normalize_ws(actual)).to eql(normalize_ws(expected))
+end
+
 Then('the result is {string}') do |expected|
     expect(@status.exitstatus).to eql(0)
     expect(@stdout.chomp).to eql(expected)
@@ -18,13 +34,13 @@ end
 
 Then('clox fails with:') do |doc_string|
     expect(@status.exitstatus).not_to eql(0)
-    expect(@stderr.chomp).to eql(doc_string)
+    expect_output(@stderr, doc_string)
 end
 
-Then('clox prints to stdout:') do |doc_string|
-    expect(@stdout.squish).to eql(doc_string.squish)
+Then('clox prints to stdout:') do |expected|
+    expect_output(@stdout, expected)
 end
 
-Then('the bytecode looks like:') do |expected_bytecode|
-    expect(@stdout.squish).to eql(expected_bytecode.squish)
+Then('the bytecode looks like:') do |expected|
+    expect_output(@stdout, expected)
 end
--- test/features/support/env.rb
+++ test/features/support/env.rb
@@ -1,1 +1,8 @@
 require 'dotenv/load'
+require 'rspec/core'
+require 'rspec/expectations'
+
+RSpec.configure do |rspec|
+    rspec.color_mode = :on
+end
+
