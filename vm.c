f72250a3f02f17a9aa08150f52c699073a2a52ad
Tjalling van der Wal <tjalling.vanderwal@nedap.com>
2022-03-01 14:31:25 +0100

    pop instead of peek in OP_JUMP_IF_FALSE

--- vm.c
+++ vm.c
@@ -274,7 +274,7 @@ static InterpretResult run(){
             }
             case OP_JUMP_IF_FALSE: {
                 uint16_t offset = READ_SHORT();
-                if (isFalsey(peek(0))) vm.ip += offset;
+                if (isFalsey(pop())) vm.ip += offset;
                 break;
             }
             case OP_LOOP: {
